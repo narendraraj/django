08fdaa265375f9494ce7e6e561a01b3167b06b5f
Naren <narenkarthi.com>
2020-08-01 18:52:35 +0200

    static dirs are confifured in Tango/ settings.py, It is working browser 127.0.0.1: 8000/static/images/image1.jpg

--- Tango/Tango/settings.py
+++ Tango/Tango/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable fpipr production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -74,6 +76,11 @@ TEMPLATES = [
 ]
 
 
+STATICFILES_DIRS = [STATIC_DIR,]
+
+
+
+
 WSGI_APPLICATION = 'Tango.wsgi.application'
 
 
@@ -125,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
